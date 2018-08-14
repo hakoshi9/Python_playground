97621291014606e04af692371efe84a43e7ce4a5
dropscar <dropscar041205@outlook.jp>
2018-08-15 01:06:33 +0900

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,13 @@
+"""タプルで遊ぶ"""
+#タプルの宣言
+tuple1 = 10, 20, 30
+print('tuple1:',tuple1)
+
+#タプルのアンパック
+tuple2 = (1, 2, 3, 4)
+print('tuple2:', tuple2)
+num1, num2, num3, num4 = tuple2
+print('num1:', num1)
+print('num2:', num2)
+print('num3:', num3)
+print('num4:', num4)
