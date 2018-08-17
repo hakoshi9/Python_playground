17b6b2ca6aed815e8450a58ab61db3ed8cafe4cb
j-hashimoto <j-hashimoto@fores.jp>
2018-08-17 09:59:16 +0900

    variable.pyのコメント変更

--- Basics/basic_grammar/variable.py
+++ Basics/basic_grammar/variable.py
@@ -1,35 +1,43 @@
 """変数宣言で遊ぶ"""
-#複数の変数を一括でそれぞれに値を持たせる
+
+
+"""複数の変数を一括でそれぞれに値を持たせる"""
 num1, num2, num3 = 5, 7, 9
 print('num1:', num1)
 print('num2:', num2)
 print('num3:', num3)
 
-#左辺の変数の数と右辺の値の数が合わないとエラーになる
+
+"""左辺の変数の数と右辺の値の数が合わないとエラーになる"""
 # num1, num2, num3 = 100
 # print(num1)
 # print(num2)
 # print(num3)
 
-#複数の変数に一括で同じ値を入れる
+
+"""複数の変数に一括で同じ値を入れる"""
 num4 = num5 = num6 = 100
 print('num4:', num4)
 print('num5:', num5)
 print('num6:', num6)
 
-#変数に*をつけると、変数の数より多い値はリストとなる
+
+"""変数に*をつけると、変数の数より多い値はリストとなる"""
 num7, *num8 = 1, 2, 3, 4, 5
 print('num7:', num7)
 print('num8:', num8) #1より先は全てリストとしてnum5に入る
 
-#１つの変数に,区切りの値を入れるとタプルとなる
+
+"""１つの変数に,区切りの値を入れるとタプルとなる"""
 num9 = 10, 11, 12
 print('num9:', num9)
 
-#上記がクラスの__init__でもできるのか試す
+
+"""上記がクラスの__init__でもできるのか試す"""
 class VarTest(object):
     def __init__(self):
         self.num10, self.num11 = 10, 20
+
 
 var_test = VarTest()
 print('num10:', var_test.num10)
