cdbf30e443e06a21d8119e29988e782b0d8a9365
dropscar <dropscar041205@outlook.jp>
2018-08-30 00:39:19 +0900

    if.py

--- Basics/flow/if.py
+++ Basics/flow/if.py
@@ -8,3 +8,35 @@ if x < 0:
 
 
 """if-else"""
+y = 10
+if x < 0:
+    print('negative')
+else:
+    print('positive')
+
+
+"""elif"""
+name = 'Ted'
+if len(name) < 5:
+    print('Less then 5 letters')
+elif len(name) < 10:
+    print('Less then 10 letters')
+else:
+    print("More then 10 letters:", len(name)-1, 'letters')
+
+
+"""入れ子"""
+a = 5
+b = 10
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is also positive')
+
+
+"""検索in"""
+my_name = 'Joshua Hashimoto'
+if 'e' in my_name:
+    print("There is a letter 'e' in my name")
+else:
+    print("There is not a letter 'e' in my name")
