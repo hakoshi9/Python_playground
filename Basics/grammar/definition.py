8c78ea906327da88d07ab6128f1aacf17af62c4b
j-hashimoto <j-hashimoto@fores.jp>
2018-09-04 13:49:32 +0900

    クロージャ―

--- Basics/grammar/definition.py
+++ Basics/grammar/definition.py
@@ -63,13 +63,44 @@ dict_arg(name='Joshua', age=22, job='Programmer', lover=False)
 
 
 """関数内関数"""
-def outer(a, b):
+def innerDefinition(a, b):
     def plus(c, d):
         return c + d
     #関数内で関数内関数を宣言する
     r = plus(a, b)
-    print('outer()の実行。中のplus()が動いている:', r)
-outer(2, 5)
+    print('innerDefinition()の実行。中のplus()が動いている:', r)
+innerDefinition(2, 5)
 
 
 """クロージャー"""
+def closerDefinition(a, b):
+    def inner():
+        return a + b
+    #実行するのではなくinner関数のオブジェクトを返す
+    return inner
+#まずはouterを実行
+f = closerDefinition(1, 2) #この時点ではinnerは実行されない
+#ここで初めて実行される
+r = f()
+print("closerDefinition()の実行:", r)
+#どういうときに使うかというと、今は実行したくないけどあとで実行したいときなど
+
+#[closerの例]
+print('クロージャ―の例------------------')
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+    return circle_area
+cal1 = circle_area_func(3.14)
+cal2 = circle_area_func(3.141592)
+print('cal1:', cal1(17))
+print('cal2:', cal2(17))
+
+
+"""デコレーター"""
+
+
+"""ラムダ"""
+
+
+"""ジェネレータ"""
