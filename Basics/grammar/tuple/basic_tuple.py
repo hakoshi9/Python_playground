2cb961cdd8d4ef49c4bc0797453a4e3e4ecede11
j-hashimoto <j-hashimoto@fores.jp>
2018-09-05 17:17:11 +0900

    tuple

---
+++ Basics/grammar/tuple/basic_tuple.py
@@ -0,0 +1,16 @@
+
+
+# タプルの宣言
+basic_tuple = 10, 20, 30
+print('basic_tuple:',basic_tuple)
+
+
+
+# タプルのアンパック
+unpack_tuple = (1, 2, 3, 4)
+print('unpack_tuple:', unpack_tuple)
+unpacked1, unpacked2, unpacked3, unpacked4 = unpack_tuple
+print('unpacked1:', unpacked1)
+print('unpacked2:', unpacked2)
+print('unpacked3:', unpacked3)
+print('unpacked4:', unpacked4)
--- Basics/grammar/tuple/tuple.py
+++ Basics/grammar/tuple/tuple.py
@@ -1,28 +1,28 @@
 """タプルで遊ぶ"""
 
 
-"""タプルの宣言"""
-tuple1 = 10, 20, 30
-print('tuple1:',tuple1)
+# """タプルの宣言"""
+# tuple1 = 10, 20, 30
+# print('tuple1:',tuple1)
 
 
 
-"""タプルのアンパック"""
-tuple2 = (1, 2, 3, 4)
-print('tuple2:', tuple2)
-num1, num2, num3, num4 = tuple2
-print('num1:', num1)
-print('num2:', num2)
-print('num3:', num3)
-print('num4:', num4)
+# """タプルのアンパック"""
+# tuple2 = (1, 2, 3, 4)
+# print('tuple2:', tuple2)
+# num1, num2, num3, num4 = tuple2
+# print('num1:', num1)
+# print('num2:', num2)
+# print('num3:', num3)
+# print('num4:', num4)
 
 
 
-"""タプルの合計値"""
-tuple3 = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-sumTuple3 = sum(tuple3)
-print('tuple3:', tuple3)
-print('tuple3の合計値:', sumTuple3)
+# """タプルの合計値"""
+# tuple3 = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# sumTuple3 = sum(tuple3)
+# print('tuple3:', tuple3)
+# print('tuple3の合計値:', sumTuple3)
 
 
 
@@ -34,16 +34,17 @@ print('リストからタプルへ:', toTuple)
 
 
 
-"""タプルをソート"""
-numTuple = 5, 3, 1, 9, 6, 7, 4, 8, 2
-#これではリストになってしまう
-sortNum = sorted(numTuple)
-#tupleに変換
-sortTuple = tuple(sortNum)
-print('numTupleをソート:', sortTuple)
+# """タプルをソート"""
+# numTuple = 5, 3, 1, 9, 6, 7, 4, 8, 2
+# #これではリストになってしまう
+# sortNum = sorted(numTuple)
+# #tupleに変換
+# sortTuple = tuple(sortNum)
+# print('numTupleをソート:', sortTuple)
 
 
 
 """タプルをスライス"""
+numTuple = 5, 3, 1, 9, 6, 7, 4, 8, 2
 sliceTuple = numTuple[1:4]
 print('index1~3までのタプル:', sliceTuple)
