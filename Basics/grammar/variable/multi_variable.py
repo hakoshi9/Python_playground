26c997d57b124b4099a5d26210cd8760cf938e50
j-hashimoto <j-hashimoto@fores.jp>
2018-09-05 16:35:00 +0900

    ファイル細分化

---
+++ Basics/grammar/variable/multi_variable.py
@@ -0,0 +1,14 @@
+
+# 複数の変数を一括で宣言する
+multi1, multi2, multi3 = 5, 7, 9
+print('multi1:', multi1)
+print('multi2:', multi2)
+print('multi3:', multi3)
+
+
+
+"""左辺の変数の数と右辺の値の数が合わないとエラーになる"""
+# num1, num2, num3 = 100
+# print(num1)
+# print(num2)
+# print(num3)
