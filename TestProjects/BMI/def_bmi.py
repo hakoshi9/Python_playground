7dbbff0929c0fffda81984d4b80cf18d1d1e25d5
j-hashimoto <j-hashimoto@fores.jp>
2018-10-05 18:25:26 +0900

    BMI

---
+++ TestProjects/BMI/def_bmi.py
@@ -0,0 +1,50 @@
+#base_level_bmi.pyの記述を、defを使ってオブジェクト指向よりにしたもの
+
+from decimal import Decimal as decimal
+
+
+def ask_weight_height():
+    weight = float(input('体重を入力してください'))
+    height = input('身長を入力してください')
+    if "." not in height:
+        height = int(height) / 100
+    elif '.' in height:
+        height = float(height)
+
+    return (weight, height)
+
+
+def show_weight_height(weight_height):
+    weight, height = weight_height
+    print('体重: {weight}kg'.format(weight=weight))
+    print('身長: {height}m'.format(height=height))
+
+
+def show_BMI(weight_height):
+    weight, height = weight_height
+    #BMI計算式: 体重kg÷(身長m)^2
+    bmi = weight / (height ** 2) #Pythonにおけるべき乗の計算は m ** n
+    #同じ変数に小数第二位までのbmiを代入
+    bmi = decimal(bmi).quantize(decimal('0.01'))
+    print('BMI: {bmi}'.format(bmi=bmi))
+
+
+def show_suitable_weight(weight_height):
+    weight, height = weight_height
+    suitable_weight = (height ** 2) * 22
+    suitable_weight = decimal(suitable_weight).quantize(decimal('0.01'))
+    print('適正体重: {suitable_weight}kg'.format(suitable_weight=suitable_weight))
+    print('適正体重より: +{over_weight}'.format(over_weight=decimal(weight) - suitable_weight))
+    
+
+
+
+
+if __name__ == '__main__':
+    weight_height = ask_weight_height()
+    show_weight_height(weight_height)
+    show_BMI(weight_height)
+    show_suitable_weight(weight_height)
+
+
+
