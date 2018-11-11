dca5b188ab3973d18bb8001299fb4c7cbfee5ced
dropscar <dropscar041205@outlook.jp>
2018-11-11 15:31:05 +0900

    辞書完了

--- Basics/dictionary/advanced_dictionary.py
+++ Basics/dictionary/advanced_dictionary.py
@@ -1,16 +1,19 @@
 """辞書のキーのみを取得する"""
+# 指定した辞書にあるキーのみを取り出す
 dict_key = {'name': 'Joshua', 'age': 22, 'job': 'programmer'}
 print('Keyのみを取得:', dict_key.keys())
 
 
 
 """辞書の値のみを取得する"""
+# 指定した辞書にあるバリューのみを取り出す
 dict_value = {'name': 'Joshua', 'age': 22, 'job': 'programmer'}
 print('Valueのみを取得:', dict_value.values())
 
 
 
 """辞書のアップデート"""
+# 辞書にある要素の中身を更新する
 update_dict = {'name': 'Joshua', 'age': 22, 'job': 'programmer'}
 # [通常]
 update_dict['age'] = 23
@@ -22,33 +25,30 @@ print('update_dictのアップデート:', update_dict)
 
 
 """辞書の中身の取得"""
+# 辞書の中身の取得にも、通常ともう1つやり方がある
 get_dict = {'name': 'Joshua', 'age': 22, 'job': 'programmer'}
-# [通常]
-print('get_dict["name"]:', get_dict["name"]) # ただし、これは存在しないキーを指定した場合エラーになる
-# [指定したキーが存在しない時にエラーにならない]
+# [通常(basic_dictionaryでもやった)]
+# ただし、これは存在しないキーを指定した場合エラーになる
+print('get_dict["name"]:', get_dict["name"])
+
+# [.get()関数の使用]
+# こちらは指定したキーが存在しない時にエラーにならない
+# 要素が存在していない時はNoneが返ってくるため、要素の検索などで安全に取り出すことができる
 print('get_dict.get("name"):', get_dict.get("name"))
 print('get_dict.get("hobby"):', get_dict.get("hobby"))
 
 
 
-"""辞書の要素を削除"""
-delete_dict = {'name': 'Joshua Hashimoto', 'age': 23, 'job': 'programmer', 'sex': 'Male'}
-# [要素を受け取って削除]
-print('popで値を受け取る:', delete_dict.pop('sex'))
-print('要素がpopされた後の辞書を確認:', delete_dict)
-# [辞書を消す]
-print('clear():', delete_dict.clear())
-
-
-
 """要素の検索"""
+#in演算子の使用
 search_dict = {'name': 'Joshua Hashimoto', 'age': 23, 'job': 'programmer', 'sex': 'Male'}
-print("'sex'キーがあるかを確認:", 'sex' in search_dict)
+print("'gender'キーがあるかを確認:", 'gender' in search_dict)
 
 
 
 """辞書のコピー"""
-copy_dict = {'name': 'Joshua Hashimoto', 'age': 23, 'job': 'programmer', 'sex': 'Male'}
+# リスト同様、辞書も参照型のため明示的に辞書をコピーしなければ元のデータが上書きされてしまう
+copy_dict = {'name': 'Joshua Hashimoto', 'age': 23, 'job': 'programmer', 'gender': 'Male'}
 recopy_dict = copy_dict.copy()
 print('--------辞書もリストと同じで参照型のためコピーが必要--------')
 print('copy_dictをコピーしたrecopy_dictのageを35に変更')
--- Basics/dictionary/basic_dictionary.py
+++ Basics/dictionary/basic_dictionary.py
@@ -13,9 +13,38 @@ str_key_dict = {'name': 'Foo Bar', 'age': 22, 'job': 'Programmer',}
 int_key_dict = {1: 'one', 5: 'five', 9: 'nine', 3: 'three', 7: 'seven',}
 var_key = '辞書用'
 mix_dict = {'work_at': 'Tokyo', 'lover': True, 'str_dict': str_key_dict, var_key: '変数をキーとして指定。変数の中身がキーとして出力される'}
+
+# 辞書の中身の取得
+# インデックスではなくキーを指定
 print(str_key_dict['job'])
 print(int_key_dict[5])
 
 # mix_dictの中身を確認
+# items()関数を使ってkeyもvalueも取り出す
 for key, value in mix_dict.items():
     print('{key}: {value}'.format(key=key, value=value))
+
+
+# 要素の追加
+add_dict = {}
+add_dict['name'] = 'Foo'
+add_dict['age'] = 35
+print('要素を追加された辞書を出力:', add_dict)
+
+
+# 要素の書き換え
+rewrite_dict = {'name': 'Foo Bar', 'age': 22, 'job': 'Programmer',}
+rewrite_dict['name'] = 'Hoge'
+rewrite_dict['age'] = 18
+rewrite_dict['job'] = 'YouTuber'
+print('要素を書き換えられた辞書を出力:', rewrite_dict)
+
+
+# 辞書の要素を削除
+# 辞書にある要素の削除
+delete_dict = {'name': 'Joshua Hashimoto', 'age': 23, 'job': 'programmer', 'gender': 'Male'}
+# [要素を受け取って削除]
+print('popで値を受け取る:', delete_dict.pop('gender'))
+print('要素がpopされた後の辞書を確認:', delete_dict)
+# [辞書そのものを消す]
+print('clear():', delete_dict.clear())
