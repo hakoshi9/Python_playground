4aa577ce2210ed7c185d2346843902286d0e7a20
dropscar <dropscar041205@outlook.jp>
2018-12-06 01:22:48 +0900

    diet_support update

--- Projects/diet_support/calculate.py
+++ Projects/diet_support/calculate.py
@@ -1,7 +1,17 @@
 
 class BodyCalculator():
-    def bmi(self):
-        """"""
+    # 体重kg * (身長m)^2
+    def bmi(self, user_weight=0, user_hegiht=0):
+        """
+        Calculates users bmi according to the users weight and height
+
+        Args:
+            weight(float/int): Users weight. Default is set at 0.
+            hegiht(float/int): Users height. Default is set at 0.
+
+        Returns:
+            bmi(float/int): The return value. Returns users calculated bmi. 
+        """
         pass
 
 
